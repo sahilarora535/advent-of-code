b34be97aff5b30972b39f641409e1f90334ab89d
Sahil Arora <12633577+sahilarora535@users.noreply.github.com>
2025-10-05 03:54:35 +0530

    refactor: remove extra print statements

--- 2024/rust/day_1/src/main.rs
+++ 2024/rust/day_1/src/main.rs
@@ -2,11 +2,9 @@ use std::fs::read_to_string;
 
 fn read_lines(filename: &str) -> Vec<String> {
     let mut result = Vec::new();
-
     for line in read_to_string(filename).unwrap().lines() {
         result.push(line.to_string())
     }
-
     result
 }
 
@@ -16,7 +14,6 @@ fn main() {
     let mut right: Vec<i32> = Vec::new();
     for i in input {
         let line: Vec<&str> = i.split_whitespace().collect();
-        println!("{:?}", line);
         let (l, r): (i32, i32) = (line[0].parse().unwrap(), line[1].parse().unwrap());
         left.push(l);
         right.push(r);
