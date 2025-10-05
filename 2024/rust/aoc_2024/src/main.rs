7ab5b3873b8682f1dbeacb14ca229a9f192e3e05
Sahil Arora <12633577+sahilarora535@users.noreply.github.com>
2025-10-05 20:49:32 +0530

    chore: add clap integration

--- 2024/rust/aoc_2024/src/main.rs
+++ 2024/rust/aoc_2024/src/main.rs
@@ -1,3 +1,14 @@
+use clap::Parser;
+
+/// Run advent-of-code 2024 for a given day.
+#[derive(Parser)]
+struct Cli {
+    /// The day to run advent-of-code, between 1 and 25
+    #[arg(short, long, default_value_t = 1)]
+    day: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    println!("Running advent-of-code 2024, day {:?}", args.day);
 }
